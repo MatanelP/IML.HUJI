5db17813ed6aaa55d489aa0f3839c95b6400d21f
MatanelP <75391764+MatanelP@users.noreply.github.com>
2022-05-09 12:40:45 +0300

    starting ex4

--- IMLearn/learners/classifiers/gaussian_naive_bayes.py
+++ IMLearn/learners/classifiers/gaussian_naive_bayes.py
@@ -1,11 +1,14 @@
 from typing import NoReturn
 from ...base import BaseEstimator
 import numpy as np
+from numpy.linalg import det, inv
+
 
 class GaussianNaiveBayes(BaseEstimator):
     """
     Gaussian Naive-Bayes classifier
     """
+
     def __init__(self):
         """
         Instantiate a Gaussian Naive Bayes classifier
@@ -39,7 +42,27 @@ class GaussianNaiveBayes(BaseEstimator):
         y : ndarray of shape (n_samples, )
             Responses of input data to fit to
         """
-        raise NotImplementedError()
+        self.classes_ = np.unique(y)
+        # calculating means + probability:
+        self.mu_ = np.zeros((len(self.classes_), X.shape[1]))
+        self.pi_ = np.zeros(len(self.classes_))
+        for k in self.classes_:
+            for i in range(X.shape[1]):
+                c_avg = 0
+                count = 0
+                for j, s in enumerate(X):
+                    if y[j] == k:
+                        count += 1
+                        c_avg += s[i]
+                self.mu_[k][i] = c_avg / count
+                self.pi_[k] = count / len(y)
+
+        # calculating variance:
+        self.vars_ = np.zeros((len(self.classes_), X.shape[1]))
+        for k in self.classes_:
+            X_k = X[y == k]
+            for feature in range(X_k.shape[1]):
+                self.vars_[k][feature] = np.var(X_k[:, feature], ddof=1)
 
     def _predict(self, X: np.ndarray) -> np.ndarray:
         """
@@ -55,7 +78,10 @@ class GaussianNaiveBayes(BaseEstimator):
         responses : ndarray of shape (n_samples, )
             Predicted responses of given samples
         """
-        raise NotImplementedError()
+        likelihood_matrix = self.likelihood(X)
+        return np.array([np.argmax(self.pi_ * likelihood_matrix[i]
+                                   / (self.pi_ @ likelihood_matrix[i]))
+                         for i, s in enumerate(X)])
 
     def likelihood(self, X: np.ndarray) -> np.ndarray:
         """
@@ -73,9 +99,17 @@ class GaussianNaiveBayes(BaseEstimator):
 
         """
         if not self.fitted_:
-            raise ValueError("Estimator must first be fitted before calling `likelihood` function")
+            raise ValueError(
+                "Estimator must first be fitted before calling `likelihood` function")
 
-        raise NotImplementedError()
+        likelihood_matrix = np.zeros((X.shape[0], len(self.classes_)))
+        for i in self.classes_:
+            d = X - self.mu_[i]
+            likelihood_matrix[:, i] = np.exp(
+                -0.5 * np.diag(d @ inv(np.diag(self.vars_[i])) @ d.T))
+            likelihood_matrix[:, i] /= np.sqrt(
+                (2 * np.pi) ** X.shape[1] * det(np.diag(self.vars_[i])))
+        return likelihood_matrix
 
     def _loss(self, X: np.ndarray, y: np.ndarray) -> float:
         """
@@ -95,4 +129,4 @@ class GaussianNaiveBayes(BaseEstimator):
             Performance under missclassification loss function
         """
         from ...metrics import misclassification_error
-        raise NotImplementedError()
+        return misclassification_error(self.predict(X), y)
--- exercises/classifiers_evaluation.py
+++ exercises/classifiers_evaluation.py
@@ -1,3 +1,6 @@
+import numpy as np
+import sklearn.discriminant_analysis
+
 from IMLearn.learners.classifiers import Perceptron, LDA, GaussianNaiveBayes
 from typing import Tuple
 from utils import *
@@ -36,16 +39,30 @@ def run_perceptron():
     Create a line plot that shows the perceptron algorithm's training loss values (y-axis)
     as a function of the training iterations (x-axis).
     """
-    for n, f in [("Linearly Separable", "linearly_separable.npy"), ("Linearly Inseparable", "linearly_inseparable.npy")]:
+
+    fig = go.Figure(
+        layout=go.Layout(title="perceptron loss"
+                               " values as a function of the"
+                               " training iterations",
+                         xaxis_title="training iterations",
+                         yaxis_title="loss values"))
+    for n, f in [("Linearly Separable", "linearly_separable.npy"),
+                 ("Linearly Inseparable", "linearly_inseparable.npy")]:
         # Load dataset
-        raise NotImplementedError()
+        X, y = load_dataset("../datasets/" + f)
 
         # Fit Perceptron and record loss in each fit iteration
         losses = []
-        raise NotImplementedError()
+        Perceptron(
+            callback=lambda fit, x, t: losses.append(fit.loss(X, y))).fit(X, y)
 
         # Plot figure of loss as function of fitting iteration
-        raise NotImplementedError()
+        fig.add_trace(
+            go.Scatter(x=list(range(1, len(losses) + 1)), y=losses,
+                       mode='lines',
+                       name=n))
+    fig.write_image(f"./ex3Plots/run_perceptron.png")
+    fig.show()
 
 
 def get_ellipse(mu: np.ndarray, cov: np.ndarray):
@@ -65,42 +82,73 @@ def get_ellipse(mu: np.ndarray, cov: np.ndarray):
         scatter: A plotly trace object of the ellipse
     """
     l1, l2 = tuple(np.linalg.eigvalsh(cov)[::-1])
-    theta = atan2(l1 - cov[0, 0], cov[0, 1]) if cov[0, 1] != 0 else (np.pi / 2 if cov[0, 0] < cov[1, 1] else 0)
+    theta = atan2(l1 - cov[0, 0], cov[0, 1]) if cov[0, 1] != 0 else (
+        np.pi / 2 if cov[0, 0] < cov[1, 1] else 0)
     t = np.linspace(0, 2 * pi, 100)
     xs = (l1 * np.cos(theta) * np.cos(t)) - (l2 * np.sin(theta) * np.sin(t))
     ys = (l1 * np.sin(theta) * np.cos(t)) + (l2 * np.cos(theta) * np.sin(t))
 
-    return go.Scatter(x=mu[0] + xs, y=mu[1] + ys, mode="lines", marker_color="black")
+    return go.Scatter(x=mu[0] + xs, y=mu[1] + ys, mode="lines",
+                      marker_color="black")
 
 
 def compare_gaussian_classifiers():
     """
     Fit both Gaussian Naive Bayes and LDA classifiers on both gaussians1 and gaussians2 datasets
     """
+
     for f in ["gaussian1.npy", "gaussian2.npy"]:
         # Load dataset
-        raise NotImplementedError()
-
+        X, y = load_dataset("../datasets/" + f)
+        lims = np.array([X.min(axis=0), X.max(axis=0)]).T + np.array([-.2, .2])
         # Fit models and predict over training set
-        raise NotImplementedError()
 
+        classifiers = [GaussianNaiveBayes().fit(X, y), LDA().fit(X, y)]
         # Plot a figure with two suplots, showing the Gaussian Naive Bayes predictions on the left and LDA predictions
         # on the right. Plot title should specify dataset used and subplot titles should specify algorithm and accuracy
         # Create subplots
         from IMLearn.metrics import accuracy
-        raise NotImplementedError()
+        accuracies = [round(accuracy(y, classifiers[i].predict(X)), 3) for i in
+                      range(len(classifiers))]
+        model_names = [f"Gaussian Naive Bayes, accuracy: {accuracies[0]}",
+                       f"LDA, accuracy: {accuracies[1]}"]
+        fig = make_subplots(rows=1, cols=2, subplot_titles=model_names,
+                            horizontal_spacing=0.01)
+        fig.update_layout(title=f"Classifying over dataset: {f}",
+                          margin=dict(t=100))  # , height= 1000, width = 500)
 
         # Add traces for data-points setting symbols and colors
-        raise NotImplementedError()
+        for i, classifier in enumerate(classifiers):
+            means = classifier.mu_
+            fig.add_traces(
+                [decision_surface(classifier.predict, lims[0], lims[1],
+                                  showscale=False),
+                 go.Scatter(x=X[:, 0], y=X[:, 1], mode="markers",
+                            showlegend=False,
+                            marker=dict(color=y,
+                                        symbol=class_symbols[y],
+                                        colorscale=class_colors(3),
+                                        line=dict(color="black",
+                                                  width=1))),
+                 # Add `X` dots specifying fitted Gaussians' means
+                 go.Scatter(x=means[:, 0], y=means[:, 1], mode="markers",
+                            name="mean",
+                            marker=dict(color="black", symbol="x", size=8)),
+                 # Add ellipses depicting the covariances of the fitted Gaussians
+                 ] +
+                [get_ellipse(mean, classifier.cov_ if i
+                else np.diag(classifier.vars_[j]))
+                 for j, mean in enumerate(means)],
+                rows=1, cols=1 + i),
 
-        # Add `X` dots specifying fitted Gaussians' means
-        raise NotImplementedError()
-
-        # Add ellipses depicting the covariances of the fitted Gaussians
-        raise NotImplementedError()
+            fig.update_layout(width=1500, height=750).update_xaxes(
+                visible=False).update_yaxes(visible=False)
+        fig.write_image(f"./ex3Plots/LDA+GNB for {f}.png")
+        fig.show()
 
 
 if __name__ == '__main__':
     np.random.seed(0)
+    GaussianNaiveBayes().fit(np.array([1,1,1,2,2,3,2,4,3,3,3,4]).reshape(-1,2), np.array([0,0,1,1,1,1]))
     run_perceptron()
     compare_gaussian_classifiers()
